a9fe29e8211a328e7da5b6daa563311d6361ca44
StoneIsDeveloper <m18146611430@163.com>
2018-07-11 18:23:48 +0800

    add

---
+++ spiderDemo/Core/INamed.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spiderDemo.Core
+{
+    /// <summary>
+    /// 名称接口定义
+    /// </summary>
+    public interface INamed
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        string Name { get; set; }
+    }
+
+    /// <summary>
+    /// 名称的抽象
+    /// </summary>
+    public abstract class Named : INamed
+    {
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name { get; set; }
+
+        public Named()
+        {
+            //var type = GetType();
+            //var nameAttribute = type.GetCustomAttribute<TaskName>();
+            //Name = nameAttribute != null ? nameAttribute.Name : type.Name;
+        }
+    }
+}
